31840323c154db30ef06caa4296fb339f0c72d55
Kate Zhao <katelyzhao@gmail.com>
2018-06-19 15:38:57 -0400

    Create C9Q03.cpp

---
+++ Chp9/Questions/C9Q03.cpp
@@ -0,0 +1,20 @@
+// C9Q03.cpp
+/* Modify MacroSideEffects.cpp so that BAND( ) works properly.  */
+
+#include "../require.h"
+#include <fstream>
+using namespace std;
+
+#define BAND(x) (((x)>5 && (x)<10) ? (x) : 0) 
+
+int main() {
+    ofstream out("macro.out");
+    assure(out, "macro.out");
+    for(int i = 4; i < 11; i++) {
+        int a = i;
+        out << "a = " << a << endl << '\t';
+        a++;
+        out << "BAND(++a)=" << BAND(a) << endl;
+        out << "\t a = " << a << endl;
+    }
+} ///:~ 
