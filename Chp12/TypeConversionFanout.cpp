5ef8d847a58864a2e569058f1228128e6514bbb1
Kate Zhao <katelyzhao@gmail.com>
2018-06-26 10:32:56 -0400

    Create TypeConversionFanout.cpp

---
+++ Chp12/TypeConversionFanout.cpp
@@ -0,0 +1,20 @@
+//: C12:TypeConversionFanout.cpp
+class Orange {};
+
+class Pear {};
+
+class Apple {
+public:
+ operator Orange() const;
+ operator Pear() const;
+};
+
+// Overloaded eat():
+void eat(Orange);
+void eat(Pear);
+
+int main() {
+ Apple c;
+//! eat(c);
+ // Error: Apple -> Orange or Apple -> Pear ???
+} ///:~ 
